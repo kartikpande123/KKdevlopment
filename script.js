be1005afad0be55f9c2619644235f5c4a4a082d3
kartikpande123 <133694115+kartikpande123@users.noreply.github.com>
2023-06-02 18:41:04 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+sh = document.getElementById("sh")
+
+function showhide(){
+    if(sh.style.display === "none"){
+        sh.style.display = "block"
+    }
+
+    else{
+        sh.style.display = "none"
+    }
+}
